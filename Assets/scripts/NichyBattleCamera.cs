a594d4210b922f88557e2850dab4e6a4e3e038d4
rose <s116741@ltisdschools.net>
2026-05-06 10:54:57 -0500

    Merging

--- Assets/scripts/NichyBattleCamera.cs
+++ Assets/scripts/NichyBattleCamera.cs
@@ -8,7 +8,7 @@ public class NichyBattleCamera : MonoBehaviour
 {
     public Camera Camera;
     public RectTransform panel;
-    public Transform level;
+    //public Transform level;
     public Rigidbody2D player;
     public float panelspeed;
     public float levelspeed;
@@ -27,8 +27,8 @@ public class NichyBattleCamera : MonoBehaviour
         ypos = (Vector3.up * yspeed * Time.deltaTime * (2 * (Camera.transform.InverseTransformPoint(player.transform.position).y + Camera.orthographicSize + 5) / (Camera.orthographicSize * 2f)));
         Camera.transform.Translate(ypos);
         panel.transform.Translate(-ypos);
-        xpos = -(Camera.transform.InverseTransformPoint(player.transform.position).x / (Camera.orthographicSize * 2));
-        panel.anchoredPosition = new Vector2(xpos / panelspeed, panel.anchoredPosition.y);
-        level.position = new Vector2(xpos / levelspeed, level.position.y);
+        xpos = (Camera.transform.InverseTransformPoint(player.transform.position).x / (Camera.orthographicSize * 2));
+        panel.anchoredPosition = new Vector2(-(xpos / panelspeed), panel.anchoredPosition.y);
+        Camera.transform.position = new Vector2(xpos / levelspeed, Camera.transform.position.y);
     }
 }
